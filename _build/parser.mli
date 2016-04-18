b699bb84863eb86e006f95e82f687f3e367586de
PLT Student <axxu3795@gmail.com>
2016-04-17 23:58:44 -0400

    Compiles with the third struct list

---
+++ _build/parser.mli
@@ -0,0 +1,40 @@
+type token =
+  | SEMI
+  | LPAREN
+  | RPAREN
+  | LBRACE
+  | RBRACE
+  | COMMA
+  | PLUS
+  | MINUS
+  | TIMES
+  | DIVIDE
+  | ASSIGN
+  | NOT
+  | EQ
+  | NEQ
+  | LT
+  | LEQ
+  | GT
+  | GEQ
+  | TRUE
+  | FALSE
+  | AND
+  | OR
+  | RETURN
+  | IF
+  | ELSE
+  | FOR
+  | INT
+  | BOOL
+  | VOID
+  | STRTYPE
+  | FUNCTION
+  | STRUCT
+  | STRING of (string)
+  | LITERAL of (int)
+  | ID of (string)
+  | EOF
+
+val program :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Ast.program
