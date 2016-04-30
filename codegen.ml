c8d806e00f51e5ca4a8ced271e70e2c46461e68a
Amarto <aar2160@columbia.edu>
2016-04-30 15:16:57 -0400

    Added declaration of thread() function to codegen. Everything compiles

--- codegen.ml
+++ codegen.ml
@@ -43,6 +43,10 @@ let translate (globals, functions) =
   let printf_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
   let printf_func = L.declare_function "printf" printf_t the_module in
 
+  let thread_t = L.function_type i32_t[| L.pointer_type i8_t |] in
+  let thread_func = L.declare_function "pthread_create" thread_t the_module in
+
+
   (* Define each function (arguments and return type) so we can call it *)
   let function_decls =
     let function_decl m fdecl =
@@ -120,6 +124,8 @@ let translate (globals, functions) =
 	    "printf" builder
       | A.Call ("print", [e])->
                 L.build_call printf_func [| (expr builder e) |] "printf" builder
+      | A.Call ("thread", [e])->
+		L.build_call thread_func [| (expr builder e) |] "pthread_create" builder
       | A.Call (f, act) ->
          let (fdef, fdecl) = StringMap.find f function_decls in
 	 let actuals = List.rev (List.map (expr builder) (List.rev act)) in
