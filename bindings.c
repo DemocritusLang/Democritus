c298c8ae9920959dd8f6cb25aa76b5b3a25275ac
Amarto <aar2160@columbia.edu>
2016-05-08 17:35:31 -0400

    THREADS NOW WORK IN SCRIPTgit status!

--- bindings.c
+++ bindings.c
@@ -1,16 +1,16 @@
 #include <pthread.h>
 
-void* default_start_routine(void *arg)
+void *default_start_routine(void *arg)
 {
 	return arg;
 }
 
-void init_thread(void *(start_routine) (void *), void *arg, int nthreads)
+void init_thread(void *(*start_routine) (void *), int arg, int nthreads)
 {
     pthread_t thread[nthreads];
     int i;
     for (i = 0; i < nthreads; i ++) {
-	pthread_create(&thread[i], NULL, start_routine, arg);
+	pthread_create(&thread[i], NULL, start_routine, NULL);
     }
 
     for (i = 0; i < nthreads; i++) {
