170645297d4578f9551f690b08b82718beebf033
Emily Pakulski <enp2111@columbia.edu>
2016-05-11 01:44:23 -0400

    Changed up get request impl a bit.

--- bindings.c
+++ bindings.c
@@ -8,35 +8,27 @@
 #include <stdlib.h>
 #define BUFSIZE 4096
 
-static void die(const char *msg)
+/*
+ * Given a URL, send a GET request.
+ */
+//void *get_request(void *url, void *filePath)
+void *request_from_server(void *url, void *filePath)
 {
-    perror(msg);
-    exit(1);
-}
-
-void *request_from_server(void *link, void *filename)
-{
-    char *serverName;
     char *serverIP;
-    char *serverPort = "80";
-    char *filePath;
-    char *fname;
-    serverName = (char *) link;
-    filePath = (char *) filename;
-    fname = (char *) filename + 1; 
-    int sock;
+    int sock;   // socket we connect to remote on
     struct sockaddr_in serverAddr;
     struct hostent *he;
-    char request[BUFSIZE], response[100 *BUFSIZE], recvbuf[BUFSIZE];
+    char recvbuf[BUFSIZE];
 
-    serverName = (char *) link;
-    if ((he = gethostbyname(serverName)) == NULL)
-	die("gethostbyname failed");
+    if ((he = gethostbyname((char *) url)) == NULL) {
+	fprintf(stderr, "gethostbyname() failed.");
+        exit(1);
+    }
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
-    {
-	die("socket failed");
+    if (sock < 0) {
+        fprintf(stderr, "socket() failed.");
+        exit(1);
     }
     serverIP = inet_ntoa(*(struct in_addr *)he->h_addr);
     memset(&serverAddr, 0, sizeof(serverAddr));
@@ -45,97 +37,81 @@ void *request_from_server(void *link, void *filename)
     serverAddr.sin_port = htons(80);
 
     int connected = connect(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
-    if(connected == -1)
-    {
-	die("connect() failed");
+    if(connected < 0) {
+	fprintf(stderr, "connect() failed.");
+        exit(1);
     }
 
     // send HTTP request
+    if (((char *) url)[strlen((char *) url) - 1] == '/') {
+        strcat(url, "index.html");
+    }
+
     snprintf(recvbuf, sizeof(recvbuf), 
-	    // note that C language concatenates adjacent string literals
-	    "GET %s HTTP/1.0\r\n"
-	    "Host: %s:%s\r\n"
-	    "\r\n",
-	    filePath, serverName, serverPort);
+            "GET %s HTTP/1.0\r\n"
+            "Host: %s:%s\r\n"
+            "\r\n",
+            url, he->h_name, "80");
     if (send(sock, recvbuf, strlen(recvbuf), 0) != strlen(recvbuf)) {
-        die("send failed");
+        fprintf(stderr, "send() failed.");
+        exit(1);
     }
 
     // wrap the socket with a FILE* so that we can read the socket using fgets()
     FILE *fd;
     if ((fd = fdopen(sock, "r")) == NULL) {
-	die("fdopen failed");
+	fprintf(stderr, "fdopen() failed.");
+        exit(1);
     }
 
-    // read the 1st line
+    /* check header for valid protocol and status code */
     if (fgets(recvbuf, sizeof(recvbuf), fd) == NULL) {
-	if (ferror(fd))
-	    die("IO error");
-	else {
-	    fprintf(stderr, "server terminated connection without response");
-	    exit(1);
-	}
+        fprintf(stderr, "server terminated connection without response.");
+        exit(1);
     } 
     if (strncmp("HTTP/1.0 ", recvbuf, 9) != 0 && strncmp("HTTP/1.1 ", recvbuf, 9) != 0) {
-	fprintf(stderr, "unknown protocol response: %s\n", recvbuf);
+	fprintf(stderr, "unknown protocol response: %s.", recvbuf);
 	exit(1);
     }
     if (strncmp("200", recvbuf + 9, 3) != 0) {
-	fprintf(stderr, "%s\n", recvbuf);
+	fprintf(stderr, "request failed with status code %s.", recvbuf);
 	exit(1);
     }
 
-    // If we're still here, it means we have a successful HTTP
-    // response (i.e., response code 200).
-
-    // Now, skip the header lines.
-    for (;;) {
+    /* ignore remaining header lines */
+    do {
 	if (fgets(recvbuf, sizeof(recvbuf), fd) == NULL) {
-	    if (ferror(fd))
-		die("IO error");
-	    else {
-		fprintf(stderr, "server terminated connection without sending file");
-		exit(1);
-	    }
+            fprintf(stderr, "server terminated connection without sending file.");
+            exit(1);
 	}
-	if (strcmp("\n", recvbuf) == 0) {
-	    // this marks the end of header lines
-	    // get out of the for loop.
-	    break;
-	}
-    }
+    } while (strcmp("\n", recvbuf) != 0);
 
-    // Now it's time to read the file.
-    // We switch to fread()/fwrite() so that we can download a binary
-    // file as well as an HTML file.
-    // (Handling binary file is not required for the assignment.)
-
-    FILE *outputFile = fopen(fname, "wb");
-    if (outputFile == NULL){
-	die("can't open output file\n");
+    /* open and read into file */
+    FILE *outputFile = fopen(filePath, "w");
+    if (outputFile == NULL) {
+	fprintf(stderr, "fopen() failed.");
+        exit(1);
     }
 
     size_t n;
     while ((n = fread(recvbuf, 1, sizeof(recvbuf), fd)) > 0) {
 	if (fwrite(recvbuf, 1, n, outputFile) != n) {
-	    die("fwrite failed");
+	    fprintf(stderr, "fwrite() failed.");
+            exit(1);
 	}
     }
-    // fread() returns 0 on EOF or on error
-    // so we need to check if there was an error.
+
     if (ferror(fd)) {
-	die("fread failed");
+	fprintf(stderr, "fread() failed.");
+        exit(1);
     }
 
-    // All done.  Clean up.
-    
     fclose(outputFile);
-
-    // closing fd closes the underlying socket as well.
     fclose(fd);
  
     return NULL;
 }
+
 void *default_start_routine(void *arg)
 {
     return arg;
