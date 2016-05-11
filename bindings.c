bd2c96ba39eebe9a7b97effd7c28c26af68b62bc
Emily Pakulski <enp2111@columbia.edu>
2016-05-11 15:22:12 -0400

    Fixed binary file reading bugs.

--- bindings.c
+++ bindings.c
@@ -90,7 +90,7 @@ void *request_from_server(void *urlVoid)
 
     // wrap the socket with a FILE* so that we can read the socket using fgets()
     FILE *fd;
-    if ((fd = fdopen(sock, "r")) == NULL) {
+    if ((fd = fdopen(sock, "rb")) == NULL) {
 	fprintf(stderr, "fdopen() failed.");
         exit(1);
     }
@@ -108,23 +108,30 @@ void *request_from_server(void *urlVoid)
 	fprintf(stderr, "request failed with status code %s.", recvbuf);
 	exit(1);
     }
-
     /* ignore remaining header lines */
     do {
+        memset(recvbuf, 0, BUFSIZE);
 	if (fgets(recvbuf, sizeof(recvbuf), fd) == NULL) {
             fprintf(stderr, "server terminated connection without sending file.");
             exit(1);
 	}
-    } while (strcmp("\n", recvbuf) != 0);
+    } while (strcmp("\r\n", recvbuf) != 0);
+
 
     char *filePathName = malloc(100);
     memset(filePathName, 0, 100);
-    strcat(filePathName, "tests/");
-    strcat(filePathName, fileName);
+    char *last_slash;
+    if ((last_slash = strrchr(filePath, '/')) != NULL) {
+        if (strlen(last_slash) == 1) {
+             strcpy(filePathName, "index.html");
+        } else {
+             strcpy(filePathName, last_slash + 1);
+        }
+    }
     
-
     /* open and read into file */
-    FILE *outputFile = fopen(filePathName, "w");
+    printf("%s\n", filePathName);
+    FILE *outputFile = fopen(filePathName, "wb");
     if (outputFile == NULL) {
 	fprintf(stderr, "fopen() failed.");
         exit(1);
@@ -132,13 +139,17 @@ void *request_from_server(void *urlVoid)
 
     size_t n;
     int total = 0;
-    while ((n = fread(recvbuf, 1, sizeof(recvbuf), fd)) > 0) {
+    memset(recvbuf, 0, BUFSIZE);
+    printf("buffer contents: %s\n", recvbuf);
+    while ((n = fread(recvbuf, 1, BUFSIZE, fd)) > 0) {
 	if (fwrite(recvbuf, 1, n, outputFile) != n) {
 	    fprintf(stderr, "fwrite() failed.");
             exit(1);
 	}
+        memset(recvbuf, 0, BUFSIZE);
 	total += n;
     }
+    fprintf(outputFile, "\n");
     fprintf(stderr, "total bytes written: %d\n", total);
     
     if (ferror(fd)) {
